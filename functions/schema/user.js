9efc5f6a34345b2d0c1047b519f3c6bd9d26511b
y4hatanok4mi <lecarosjc@gmail.com>
2024-06-08 16:10:09 +0800

    Update user.js

--- functions/schema/user.js
+++ functions/schema/user.js
@@ -13,10 +13,4 @@ const userSchema = new Schema({
     }
 });
 
-userSchema.pre("save", function (next) {
-    const username = this.name.toLowerCase().replace(/\s/g, "");
-    this.username = username;
-    next();
-  });
-
 module.exports = userSchema;
