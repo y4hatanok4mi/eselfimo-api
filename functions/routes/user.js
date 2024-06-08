3ea8f80cd65e23ac387ed43e325fd5f047859c53
y4hatanok4mi <lecarosjc@gmail.com>
2024-06-08 15:00:21 +0800

    Update user.js

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -5,23 +5,25 @@ const Task = require('../models/user');
 
 // Signup route
 router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-
     try {
-        // Check if username already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists. Please choose a different one.' });
+        //validate the request
+        if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: "Username and Password are required" });
         }
-
-        // Create new user
-        const newUser = new User({ username, password });
-        await newUser.save();
-
-        res.status(201).json({ message: 'User created successfully.' });
+        //check if author already exists
+        const existingUser = await User.findOne({
+        name: req.body.name,
+        });
+        if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+        }
+    
+        const user = new User(req.body);
+        const newUser = await user.save();
+        res.status(201).json({ message: "Author created", user: newUser });
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error.' });
-    }
+        res.status(500).json({ message: error.message });
+        }
 });
 
 // Login route
