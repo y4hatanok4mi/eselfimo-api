760f6dde79c2c2393460a6884f24cfcd1d51de7e
y4hatanok4mi <lecarosjc@gmail.com>
2024-06-08 15:30:22 +0800

    Update user.js

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -7,25 +7,25 @@ const Task = require('../models/task');
 router.post('/signup', async (req, res) => {
     const { username, password } = req.body;
     try {
-        //validate the request
-        if (!req.body.username || !req.body.password) {
-        return res.status(400).json({ message: "Username and Password are required" });
+        // Validate the request
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and Password are required" });
         }
-        //check if author already exists
-        const existingUser = await User.findOne({
-        username: req.body.username,
-        });
+        // Check if user already exists
+        const existingUser = await User.findOne({ username });
         if (existingUser) {
-        return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ message: "User already exists" });
         }
-    
-        const user = new User(req.body);
-        const newUser = await user.save();
-        res.status(201).json({ message: "Author created", user: newUser });
+
+        // Create new user
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.status(201).json({ message: "User created", user: newUser });
     } catch (error) {
         res.status(500).json({ message: error.message });
-        }
+    }
 });
+
 
 // Login route
 router.post('/login', async (req, res) => {
