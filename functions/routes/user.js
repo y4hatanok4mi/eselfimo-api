c8cc0e2b5eef9e740d3bd6c0312cbcf280340cbf
y4hatanok4mi <lecarosjc@gmail.com>
2024-06-08 14:44:28 +0800

    Update user.js

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const Task = require('../models/task');
+const Task = require('../models/user');
 
 // Signup route
 router.post('/signup', async (req, res) => {
