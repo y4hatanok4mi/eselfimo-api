2864a37b7309a2fe590a41e3d451ee5199b552b4
y4hatanok4mi <lecarosjc@gmail.com>
2024-06-08 15:47:22 +0800

    Update user.js

--- functions/models/user.js
+++ functions/models/user.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 const userSchema = require("../schema/user");
-const taskSchema = require("../schema/task");
 
 const userModel = mongoose.model("User", userSchema);
-const taskModel = mongoose.model("Task", taskSchema);
 
 module.exports = userModel;
-module.exports = taskModel;
